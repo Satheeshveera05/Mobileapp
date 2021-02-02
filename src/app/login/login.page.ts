7804e7fd93b69b16af23c339d6a95b5f356b9857
Satheeshveera05 <satheeshv@boodskap.io>
2021-02-02 14:00:30 +0530

    modify login style

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,24 +9,40 @@ import { LoadingController } from '@ionic/angular';
 })
 export class LoginPage {
 
-  constructor(public router:Router ,public loadingController: LoadingController) { }
+  constructor(public router:Router ,public loadingController: LoadingController,public alertController: AlertController) { }
  
 
   // logins(){
   //   this.router.navigate(['/home'])
   // }
-  
-  async presentLoading() {
-    const loading = await this.loadingController.create({
+  username : string =""
+  password : string =""
+  async presentAlert() {
+    const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
-      message: 'Please wait...',
-      duration: 2000
+      header: 'Alert',
+      message: 'Invalid User...',
+      buttons: ['OK']
     });
-    await loading.present();
-    this.router.navigate(['/home'])
-
-    const { role, data } = await loading.onDidDismiss();
-    console.log('Loading dismissed!');
+    await alert.present();
+  }
+  async presentLoading() {
+    if(this.username == "boodskap" && this.password == "welcome123" ){
+      const loading = await this.loadingController.create({
+        cssClass: 'my-custom-class',
+        message: 'Please wait...',
+        duration: 2000
+      });
+      await loading.present();
+      this.router.navigate(['/home'])
+  
+      const { role, data } = await loading.onDidDismiss();
+      console.log('Loading dismissed!');
+    }else{
+      this.presentAlert();
+      this.router.navigate(['/login'])
+    }
+    
   }
 
 }
